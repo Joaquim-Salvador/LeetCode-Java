2edf3882520b3dca4b86bfaabcd532d15edb07ec
Joaquim-Salvador <joaquimlennertz@gmail.com>
2026-07-18 12:15:00 -0300

    exercicio finalizado e funcional, mas ele não está ideal para produção por conter 4 for, vou melhorar ele

--- FourSum.java
+++ FourSum.java
@@ -17,43 +17,30 @@
 // Output: [[2,2,2,2]]
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<Integer> numeros = new ArrayList<>();
         List<List<Integer>> listaCerta = new ArrayList<>();
-        int soma = 0;
-        int somaLista = 0;
-        
-        for(int i = 0; i < nums.length; i++){
-            int x = i;
-            while (x < nums.length) { 
-                soma += nums[x];
-                System.out.println("Soma: " + soma + " += " + nums[x]);
-                System.out.println();
-                if(soma <= target && numeros.size() <= 4){
-                    numeros.add(nums[x]);
-                } else {
-                    soma -= nums[x];
-                }
-                System.out.println(numeros);
 
-                if(numeros.size() == 4){
-                    for(int n : numeros){
-                        somaLista += n;
+        for(int i = 0; i < nums.length; i++){    
+            for(int j = i + 1; j < nums.length; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    for(int l = k + 1; l < nums.length; l++){
+                        if((nums[i] + nums[j] + nums[k] + nums[l]) == target){
+                            numeros.addAll(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                            Collections.sort(numeros);
+                            if(!listaCerta.contains(numeros)){
+                                listaCerta.add(new ArrayList<>(numeros));
+                            }
+                            numeros = new ArrayList<>();
+                        }
                     }
-                    if(somaLista == target){
-                        listaCerta.add(new ArrayList<>(numeros));
-                    }
-                    somaLista = 0;
-                    numeros = new ArrayList<>();
-            
                 }
-                x+=1;
-                
             }
-            soma = 0;
         }
         return listaCerta;
     }
@@ -61,27 +48,6 @@ class FourSum {
     public static void main (String[] args) {
         FourSum teste = new FourSum();
         
-        System.out.print(teste.fourSum(new int[] {1,0,-1,0,-2,2}, 0));
+        System.out.print(teste.fourSum(new int[] {2,2,2,2,2}, 8));
     }
 }
-
-            // int a = nums[i];
-            // int b = nums[(i + 1) % nums.length];
-            // int c = nums[(i + 2) % nums.length];
-            // int d = nums[(i + 3) % nums.length];
-
-            // numeros.add(a);
-            // numeros.add(b);
-            // numeros.add(c);
-            // numeros.add(d);
-
-            //             soma = 0;
-            // for (int n : numeros){
-            //     soma += n;
-            // }
-
-            // if(soma == target){
-            //     listaCerta.add(new ArrayList<>(numeros));
-            //     numeros = new ArrayList<>();
-            //     soma = 0;
-            // }
